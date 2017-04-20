336458a3ff6e7ade7fa91c46b7e08361bc2786c2
komaltiwari20 <komal.tiwari@ves.ac.in>
2017-04-20 19:15:04 +0530

    Create main.php

---
+++ main.php
@@ -0,0 +1,10 @@
+<ul>
+    <li><a href="/owncloud/apps/another/templates/try.php">First level entry</a></li>
+    <li>
+        <a href="/../../owncloud/index.php/settings/admin/log/download">First level container</a>
+        <ul>
+            <li><a href="#">Second level entry</a></li>
+            <li><a href="#">Second level entry</a></li>
+        </ul>
+    </li>
+</ul>
